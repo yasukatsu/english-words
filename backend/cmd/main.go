e228b82572c062f2e10d001eadba9e7e212e348e
Yasukastsu.A <handbuuuu@gmail.com>
2020-04-28 00:09:46 +0900

    del

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"net/http"
 
@@ -12,21 +11,6 @@ import (
 	pos "y_ara/english-words/backend/internal/app/postgres"
 )
 
-// Words ...
-type Words []struct {
-	Meanings          string `json:"Meanings"`
-	EnglishDefinition string `json:"EnglishDefinition"`
-	JTranslation      string `json:"JTranslation"`
-}
-
-var (
-	path  = "./resourse/datas.json"
-	pathA = "./resourse/a.json"
-	pathB = "./resourse/b.json"
-	pathC = "./resourse/c.json"
-	pathD = "./resourse/d.json"
-)
-
 func main() {
 	allowedOrigins := handlers.AllowedOrigins([]string{"http://localhost:8080"})
 	allowedMethods := handlers.AllowedMethods([]string{"GET", "POST", "DELETE", "PUT"})
@@ -34,14 +18,11 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/list", listWords)
 
-	fmt.Println("test")
-
 	log.Fatal(http.ListenAndServe(":8000", handlers.CORS(allowedOrigins, allowedMethods)(r)))
 }
 
 func listWords(w http.ResponseWriter, r *http.Request) {
 	words, err := pos.ListWords()
-	fmt.Println("test2")
 
 	if err != nil {
 		log.Fatalf("ListWords err is %v\n", err)
@@ -55,23 +36,3 @@ func listWords(w http.ResponseWriter, r *http.Request) {
 	}
 	w.Write(byteWords)
 }
-
-// func list(w http.ResponseWriter, r *http.Request) {
-// 	body, err := readFile(path)
-// 	if err != nil {
-// 		log.Fatalf("ReadFile err is %v", err)
-// 		return
-// 	}
-
-// 	w.Write(body)
-// }
-
-// func readFile(path string) ([]byte, error) {
-// 	raw, err := ioutil.ReadFile(path)
-// 	if err != nil {
-// 		log.Fatalf("ReadFile err is %v", err)
-// 		return nil, err
-// 	}
-// 	body := bytes.TrimPrefix(raw, []byte("\xef\xbb\xbf"))
-// 	return body, nil
-// }

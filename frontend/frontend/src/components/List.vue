4823f8d8153b9e92e7883888ef1375b96927c79e
Yasukastsu.A <handbuuuu@gmail.com>
2020-04-28 12:37:08 +0900

    fix frontend-container

---
+++ frontend/frontend/src/components/List.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="list">
+    <ul>
+      <li v-for="word in words" :key="word.Meanings">{{ word }}</li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+export default {
+  name: 'List',
+  data () {
+    return {
+      words: []
+    }
+  },
+  created () {
+    axios.get('localhost:8000/list').then(res => {
+      this.words = res.data
+    })
+  }
+}
+</script>
+
+<style scoped>
+h1,
+h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
